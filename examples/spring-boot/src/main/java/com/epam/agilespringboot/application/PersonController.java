ee7e11da5143e353fe6c2e6a43ea0c562c560356
Oresztesz Margaritisz <v-omargaritisz@hotels.com>
2019-08-02 09:08:42 +0200

    Added swagger and JPA with simple repository

--- examples/spring-boot/src/main/java/com/epam/agilespringboot/application/PersonController.java
+++ examples/spring-boot/src/main/java/com/epam/agilespringboot/application/PersonController.java
@@ -1,17 +1,24 @@
 package com.epam.agilespringboot.application;
 
 import com.epam.agilespringboot.domain.Person;
+import com.epam.agilespringboot.infrastructure.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 public class PersonController {
 
+    @Autowired
+    private PersonRepository personRepository;
+
     @GetMapping("/person/{id}")
-    Mono<Person> findById(@PathVariable String id) {
-        var person = new Person(1, "John", "Smith");
-        return Mono.just(person);
+    ResponseEntity<Person> findById(@PathVariable String id) {
+        final var person = personRepository.findById(Integer.valueOf(id));
+        return person.map(result -> new ResponseEntity<>(result, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 }
